512ee55e68cc4aabf91b5d1046db118b8648f731
Emil Backlund <emil_backi_@hotmail.com>
2023-01-06 03:29:16 +0100

    make the html, add pictures, install packages, styling

---
+++ tailwind.config.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ['./src/*.{html,js}', './src/js/*.js'],
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
